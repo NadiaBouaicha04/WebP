69c5287e3c612ebc51b4a06cff5942ed56a15870
louay10 <louay.nagati@esprit.tn>
2025-09-22 14:31:49 +0200

    update code

--- server.py
+++ server.py
@@ -13,6 +13,30 @@ model = joblib.load('immobilier_tunisie_model.pkl')
 with open('model_metadata.json', 'r') as f:
     model_metadata = json.load(f)
 
+@app.route('/average_prices', methods=['GET'])
+def all_average_prices():
+    try:
+        df = pd.read_excel("Full Combined Cleaned Data.xlsx")
+
+        # Nettoyer les gouvernorats doublons ou incohérents
+        df['Governorate'] = df['Governorate'].str.strip().replace({
+            'La Manouba': 'Manouba',
+            'Tunis ': 'Tunis'
+        })
+
+        # Supprimer les prix <= 0 ou trop élevés
+        df = df[(df['Price (TND)'] > 0) & (df['Price (TND)'] < 1e7)]
+
+        # Calcul des stats
+        avg_prices = df.groupby('Governorate')['Price (TND)'].agg(['min','max','mean']).reset_index()
+        avg_prices = avg_prices.round(0)  # arrondir à l'entier le plus proche
+
+        result = avg_prices.to_dict(orient='records')
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
